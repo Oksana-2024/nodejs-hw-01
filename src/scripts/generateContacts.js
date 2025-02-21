5c787575b7628620f4eb1421f4397f6510fa6ea9
Oksana <oksanavasilevna77@gmail.com>
2025-02-21 13:29:10 +0200

    add generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,14 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+  for (let index = 0; index < number; index++) {
+    const newContact = createFakeContact();
+    contacts.push(newContact);
+  }
+  writeContacts(contacts);
+};
 
 generateContacts(5);
